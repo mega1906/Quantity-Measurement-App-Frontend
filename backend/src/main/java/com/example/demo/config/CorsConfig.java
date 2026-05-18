c83030b3cf2ad95558e901900027603e04c8012c
mega1906 <mega.dioscuri@gmail.com>
2026-05-19 00:50:01 +0530

    added cors config file

---
+++ backend/src/main/java/com/example/demo/config/CorsConfig.java
@@ -0,0 +1,18 @@
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
