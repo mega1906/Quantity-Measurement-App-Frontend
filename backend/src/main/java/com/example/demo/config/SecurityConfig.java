00b568f6b19eb60e1c05dede33e3da371bf417fc
mega1906 <mega.dioscuri@gmail.com>
2026-05-19 10:45:09 +0530

    updated security config

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -104,7 +104,8 @@ public class SecurityConfig {
             Arrays.asList(
                 "http://localhost:3000",
                 "http://localhost:3001",
-                "http://3.107.229.154:3001"
+                "http://3.107.229.154:3001",
+                "http://32.236.14.140:3001"
             )
         );
 
